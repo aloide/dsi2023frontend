ddb7534daf165d8b06c761a4ba599692c6b3d36e
aloide <ale_axel_molina@hotmail.com>
2023-05-25 18:28:10 -0300

    fix: app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 //import React from "react";
 import "./App.css";
 
-import ConsultarEncuesta from "./Pages/ConsultarEncuesta";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import NotFound from "./Pages/NotFound";
-
+import Splash from "./Pages/Splash";
+import ConsultarEncuesta from "./Pages/ConsultarEncuesta";
 
 function App() {
   let Component;
   switch (window.location.pathname) {
     case "/":
-      Component = <ConsultarEncuesta/>
+      Component = <Splash />;
       break;
-      default:
-        Component = <NotFound></NotFound>
-    
+      case "/ConsultarEncuesta":
+        Component = <ConsultarEncuesta />;
+        break;
+    default:
+      Component = <NotFound></NotFound>;
   }
   return (
     <div>
-      <Header>
-      </Header>
+      <Header></Header>
       <div className="container">{Component}</div>
 
       <Footer></Footer>
