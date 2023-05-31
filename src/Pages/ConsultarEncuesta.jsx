f654b078bbd916af3752c7890c0ccd627610da29
aloide <ale_axel_molina@hotmail.com>
2023-05-31 11:35:08 -0300

    refactor: nuevo hook y logica en el boton de buscar llamados

--- src/Pages/ConsultarEncuesta.jsx
+++ src/Pages/ConsultarEncuesta.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 import LlamadasConEncuestas from "../components/LlamadasConEncuestas/LlamadasConEncuestas";
+import useLlamadasConEncuestas from "../../Hooks/useLlamadasConEncuestas";
 
 import "react-datepicker/dist/react-datepicker.css";
 import "./ConsultarEncuesta.css";
@@ -9,22 +10,56 @@ const ConsultarEncuesta = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
+  const { llamadas, datosLlamada, idLlamada, obtenerDatosLlamada } =
+    useLlamadasConEncuestas(startDate, endDate);
+
+  const handleBuscarLlamadas = () => {
+    if (endDate > startDate) {
+      obtenerDatosLlamada(startDate, endDate);
+    } else {
+      // Mostrar mensaje de error o realizar acciones adicionales si las fechas no son válidas
+      console.error("No se puede obtener llamadas, corrige las fechas...");
+    }
+  };
 
   return (
     <div className="form-principal">
       <div className="form-group">
         <h3>Indicar Periodo</h3>
-        <label>Fecha de inicio</label>
-        <DatePicker dateFormat={"dd/MM/yyyy"} selected={startDate} onChange={(date) => setStartDate(date)} />
 
-        <label>Fecha de fin
-        <DatePicker dateFormat={"dd/MM/yyyy"} selected={endDate} onChange={(date) => setEndDate(date)} />
+        <label>
+          Fecha de inicio
+          <DatePicker
+            dateFormat="dd/MM/yyyy"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
         </label>
 
-        <button className="btn btn-primary">Buscar llamadas</button>
+        <label>
+          Fecha de fin
+          <DatePicker
+            dateFormat="dd/MM/yyyy"
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+          />
+        </label>
 
-        <LlamadasConEncuestas />
+        <button
+          className="btn btn-primary"
+          id="btn-buscar-llamadas"
+          onClick={handleBuscarLlamadas}
+          disabled={endDate <= startDate}
+        >
+          Buscar llamadas
+        </button>
 
+        <LlamadasConEncuestas
+          llamadas={llamadas}
+          datosLlamada={datosLlamada}
+          idLlamada={idLlamada}
+          obtenerDatosLlamada={obtenerDatosLlamada}
+        />
       </div>
     </div>
   );
--- src/components/LlamadasConEncuestas/LlamadasConEncuestas.jsx
+++ src/components/LlamadasConEncuestas/LlamadasConEncuestas.jsx
@@ -1,80 +1,32 @@
-import { useEffect, useState } from "react";
 import "./LlamadasConEncuestas.css";
-import axios from "axios";
 import UseDatosLlamada from "../../Hooks/UseDatosLlamada";
 import Botonera from "../Botonera/Botonera";
 import BtnCancelar from '../BtnCancelar/BtnCancelar';
 import BtnDescargarCsv from "../BtnDescargarCsv/BtnDescargarCsv";
 import BtnImprimir from "../BtnImprimir/BtnImprimir";
+import useLlamadasConEncuestas from "../../Hooks/UseLlamadasConEncuestas";
 
 const LlamadasConEncuestas = () => {
-  const [llamadas, setLlamadas] = useState([]);
-  const [datosLlamada, setdatosLlamada] = useState({});
-  const [idLlamada, setidLlamada] = useState(1)
-
-  const obtenerLlamadas = async () => {
-    const urlApi =
-      "http://rovtest01.ddns.net:27015/api/llamadas-cn-encuesta-resp";
-
-    try {
-      const resp = await axios.get(urlApi);
-      setLlamadas(resp.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const obtenerDatosLlamadas = async (id) => {
-    setidLlamada(id)
-    //alert(id);
-    const urlApi = `http://rovtest01.ddns.net:27015/api/llamada/${id}`;
-
-    try {
-      const res = await axios.get(urlApi);
-      setdatosLlamada(res.data);
-      //console.log(res.data.respuestas)
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { llamadas, datosLlamada, idLlamada, obtenerDatosLlamada } = useLlamadasConEncuestas();
 
   const renderizarTBody = () => {
-    let tbody = [];
-
-    llamadas.map((ll) => {
-      //console.log(ll.descripcionOperador);
-      tbody.push(
-        <tr key={ll.llamadaId} >
-          <td> {ll.llamadaId}</td>
-          <td> {ll.descripcionOperador}</td>
-          <td> {ll.duracion}</td>
-          <td>
-            <button className="btn btn-primary" onClick={() => obtenerDatosLlamadas(ll.llamadaId)}>
-              Seleccionar
-            </button>
-          </td>
-        </tr>
-      );
-    });
-
-    return tbody;
+    return llamadas.map((llamada) => (
+      <tr key={llamada.llamadaId}>
+        <td>{llamada.llamadaId}</td>
+        <td>{llamada.descripcionOperador}</td>
+        <td>{llamada.duracion}</td>
+        <td>
+          <button className="btn btn-primary" onClick={() => obtenerDatosLlamada(llamada.llamadaId)}>
+            Seleccionar
+          </button>
+        </td>
+      </tr>
+    ));
   };
-
-  const renderizarDatosLlamada = () => {
-    const div = []
-    div.push(
-        <UseDatosLlamada llamada={datosLlamada} rtas={datosLlamada.respuestas}></UseDatosLlamada>
-    )
-    return div
-  };
-
-  useEffect(() => {
-    obtenerLlamadas();
-  }, []);
 
   return (
     <div id="LlamadasConEncuestas">
-      <h3>Llamadas con Encuestas </h3>
+      <h3>Llamadas con Encuestas</h3>
 
       <div id="Llamadas">
         <table className="table table-hover">
@@ -89,14 +41,15 @@ const LlamadasConEncuestas = () => {
         </table>
       </div>
 
-      <div id="datosLlamada">{renderizarDatosLlamada()}</div>
+      <div id="datosLlamada">
+        <UseDatosLlamada {...datosLlamada}></UseDatosLlamada>
+      </div>
 
       <Botonera>
-        <BtnDescargarCsv id={idLlamada} ></BtnDescargarCsv>
+        <BtnDescargarCsv id={idLlamada}></BtnDescargarCsv>
         <BtnImprimir></BtnImprimir>
         <BtnCancelar></BtnCancelar>
       </Botonera>
-      
     </div>
   );
 };
